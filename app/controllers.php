171801a227a09f2bbb8edc966adec708a68d3d9d
nattkua <info@nathamilton.com>
2016-05-03 17:51:49 -0500

    added in a few functions to the controller and model, can now do basic CRUD

--- app/controllers.php
+++ app/controllers.php
@@ -15,6 +15,82 @@
       echo json_encode($todo_list);
     }
 
+    // grabing all completed todos
+    public function completed() {
+      $todo_list = Todo::get_completed();
+
+      echo json_encode($todo_list);
+    }
+
+    // grabing a todo by id
+    public function get() {
+      if (!isset($_GET['id'])) {
+        return call('error');
+      }
+
+      $id = $_GET['id'];
+      $todo = Todo::get_todo($id);
+
+      echo json_encode($todo);
+    }
+
+    // create a new todo
+    public function create() {
+      if (!isset($_GET['title']) || !isset($_GET['due_date'])) {
+        return call('error');
+      }
+
+      $title = $_GET['title'];
+      $due_date = $_GET['due_date'];
+      $new_todo = Todo::create_todo($title,$due_date);
+      $todo = Todo::get_todo($new_todo);
+
+      echo json_encode($todo);
+    }
+
+    // deleting a todo by id
+    public function delete() {
+      if (!isset($_GET['id'])) {
+        return call('error');
+      }
+
+      $id = $_GET['id'];
+      $deleted_todo = Todo::delete_todo($id);
+
+      echo json_encode($deleted_todo);
+    }
+
+    // Setting todo as completed by id
+    public function done() {
+      if (!isset($_GET['id'])) {
+        return call('error');
+      }
+
+      $id = $_GET['id'];
+      $finished_todo = Todo::completed_todo($id);
+      $todo = Todo::get_todo($finished_todo);
+
+      echo json_encode($todo);
+    }
+
+    // Resetting todo to not complete
+    public function reset() {
+      if (!isset($_GET['id'])) {
+        return call('error');
+      }
+
+      $id = $_GET['id'];
+      $reset_todo = Todo::reset_todo($id);
+      $todo = Todo::get_todo($reset_todo);
+
+      echo json_encode($todo);
+    }
+
+    // Error could be better but you know
+    public function error() {
+      $error = array('error'=>'There has been an error with your request dudeman');
+      echo json_encode($error);
+    }
 
   }
 
--- app/models.php
+++ app/models.php
@@ -21,7 +21,7 @@
       $this->complete = ($complete == 0 ? false : true);
     }
 
-    // Todo's fun fun functions !!!
+    // Returns all upcoming todos
     public static function get_upcoming() {
       $todo_list = [];
       $db = Database::getInstance();
@@ -32,6 +32,77 @@
       }
 
       return $todo_list;
+    }
+
+    // Returns all completed todos
+    public static function get_completed() {
+      $todo_list = [];
+      $db = Database::getInstance();
+      $request = $db->query('SELECT * FROM todo WHERE completed = 1 ORDER BY due_date ASC LIMIT 6');
+
+      foreach ($request->fetchAll() as $todo) {
+        $todo_list[] = new Todo($todo['id'],$todo['title'],$todo['due_date'],$todo['created_date'],$todo['overdue'],$todo['completed']);
+      }
+
+      return $todo_list;
+    }
+
+    // Returns a todo by id
+    public static function get_todo($id) {
+      $db = Database::getInstance();
+      $id = intval($id);
+      $request = $db->prepare('SELECT * FROM todo WHERE id = :id');
+      $request->execute(array('id'=>$id));
+      $todo = $request->fetch();
+
+      return new Todo($todo['id'],$todo['title'],$todo['due_date'],$todo['created_date'],$todo['overdue'],$todo['completed']);
+    }
+
+    // Returns the id of a newly created todo
+    public static function create_todo($title, $due_date) {
+      $db = Database::getInstance();
+      $created_date = Date('Y-m-d');
+      $created_date = strval($created_date);
+      $due_date = strval($due_date);
+      $vals = array(':id'=>NULL, ':title'=>$title, ':due_date'=>$due_date, ':created_date'=>$created_date, ':overdue'=>0, ':completed'=>0);
+      $request = $db->prepare("INSERT INTO todo VALUES (:id, :title, :due_date, :created_date, :overdue, :completed)");
+      $request->execute($vals);
+      $id = $db->lastInsertId();
+
+      return $id;
+    }
+
+    // Delete a todo
+    public static function delete_todo($id) {
+      $db = Database::getInstance();
+      $id = intval($id);
+      $vals = array('id'=>$id);
+      $request = $db->prepare('DELETE FROM todo WHERE id = :id');
+      $request->execute($vals);
+
+      return $id;
+    }
+
+    // Complete a todo
+    public static function completed_todo($id) {
+      $db = Database::getInstance();
+      $id = intval($id);
+      $vals = array('id'=>$id);
+      $request = $db->prepare('UPDATE todo SET completed=1 WHERE id = :id');
+      $request->execute($vals);
+
+      return $id;
+    }
+
+    // Reset a completed todo
+    public static function reset_todo($id) {
+      $db = Database::getInstance();
+      $id = intval($id);
+      $vals = array('id'=>$id);
+      $request = $db->prepare('UPDATE todo SET completed=0 WHERE id = :id');
+      $request->execute($vals);
+
+      return $id;
     }
 
   }
