b3a4bfc8942b46a8ad1d0ed4284a1ccf2fcdb777
nattkua <info@nathamilton.com>
2016-05-11 19:13:15 -0500

    updated my database connection to connect to CLEARDB on Heroku

--- app/database.php
+++ app/database.php
@@ -12,7 +12,8 @@ class Database
   public static function getInstance() {
     if (!isset(self::$instance)) {
       $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-      self::$instance = new PDO('mysql:host=localhost;dbname=todo2', 'root', 'root', $pdo_options);
+      // local settings self::$instance = new PDO('mysql:host=localhost;dbname=todo2', 'root', 'root', $pdo_options);
+      self::$instance = new PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_e87991e6ce48121', 'b3661b4d719eb1', '708fed96', $pdo_options); // heroku settings
     }
     return self::$instance;
   }
